ae5e8a6ab973a47795420d5e0e8dff7ba18ba48e
Hunter Caron <huntercaron@me.com>
2017-03-03 20:53:52 -0500

    pushing broken code

--- build/js/min/main-min.js
+++ build/js/min/main-min.js
@@ -1,39 +1,192 @@
 var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
 
 var times = [];
+var eventHeight = 30;
 
-moment().format();
+var startOfWeek = moment().startOf('week');
+var endOfWeek = moment().endOf('week');
 
-function CalEvent(props) {
-    let eventInfo;
-    if (props.time.status) {
-        eventInfo = _jsx("p", {}, void 0, props.time.statusString);
-    } else {
-        eventInfo = _jsx("p", {}, void 0, props.time.statusString);
+var days = [];
+var day = startOfWeek;
+
+while (day <= endOfWeek) {
+    days.push(day.toDate());
+    day = day.clone().add(1, 'd');
+}
+
+console.log(days);
+
+class CalEvent extends React.Component {
+    render() {
+        let eventInfo;
+        if (this.props.time.status) {
+            eventInfo = _jsx('p', {}, void 0, this.props.time.statusString);
+        } else {
+            eventInfo = _jsx('p', {}, void 0, this.props.time.statusString);
+        }
+        return _jsx('div', {
+            style: this.props.style,
+            className: `sched-row cal-event-box`
+        }, void 0, _jsx('div', {
+            className: `cal-event ${this.props.time.statusString}`
+        }, void 0, eventInfo));
     }
-    return _jsx("div", {
-        className: `sched-row cal-event ${props.time.statusString}`
-    }, void 0, eventInfo);
 }
 
-function Studio(props) {
-    return _jsx("div", {
-        className: "studio-col"
-    }, void 0, props.data.days[0].times.map(function (time, i) {
-        if (i > 11) return _jsx(CalEvent, {
-            time: time,
-            className: `sched-row ${time.status}`,
-            status: time.status
-        });
-    }));
+class Studio extends React.Component {
+    render() {
+        let scope = this;
+        let count = 1;
+        console.log(scope.props.data);
+        return _jsx('div', {
+            className: 'studio-col'
+        }, void 0, this.props.data.days[0].times.map(function (time, i) {
+            if (i > 11) {
+                if (i != scope.props.data.days[0].times.length - 1) {
+                    if (scope.props.data.days[0].times[i + 1].statusString != time.statusString || time.statusString == "Closed") {
+
+                        let eventStyle = {
+                            height: count * eventHeight + "px"
+                        };
+                        count = 1;
+
+                        return _jsx(CalEvent, {
+                            time: time,
+                            className: `sched-row ${time.status}`,
+                            status: time.status,
+                            style: eventStyle
+                        });
+                    } else {
+                        count++;
+                    }
+                }
+            }
+        }));
+    }
 }
 
-class Calendar extends React.Component {
+class CalendarBody extends React.Component {
+    render() {
+        return _jsx('div', {
+            className: 'sched-main sched-col'
+        }, void 0, this.props.viewAll ? this.props.data.map(function (studio, i) {
+            return _jsx(Studio, {
+                className: 'sched-row',
+                data: studio
+            });
+        }) : _jsx(Studio, {
+            className: 'sched-row',
+            data: this.props.data[this.props.selected]
+        }));
+    }
+
+}
+
+class CalTimes extends React.Component {
+    render() {
+        let calTimeFormat = moment().hour(6).minute(0);
+        let calTimeNum = 6;
+        let calTimes = [];
+
+        while (calTimeNum <= 24) {
+            calTimes.push({ "formatted": calTimeFormat.format("h:mma"), "number": calTimeNum });
+            calTimeNum = calTimeNum + 0.5;
+            calTimeFormat.add(30, 'm');
+        }
+
+        return _jsx('div', {
+            className: 'sched-col'
+        }, void 0, calTimes.map(function (time, i) {
+            let hourType = !Number.isInteger(time.number) ? "half-hour" : "full-hour";
+            return _jsx('div', {
+                className: `time sched-row ${hourType}`,
+                id: `${time.number}-time`
+            }, void 0, time.formatted);
+        }));
+    }
+}
+
+var _ref = _jsx('div', {
+    className: 'day'
+}, void 0);
+
+class SelectorDay extends React.Component {
+    render() {
+        return _ref;
+    }
+}
+
+var _ref2 = _jsx('br', {});
+
+class SelectorWeek extends React.Component {
+    render() {
+        let startOfWeek = moment().startOf('week');
+        let endOfWeek = moment().endOf('week');
+
+        let days = [];
+        let day = this.props.startDate;
+
+        while (day <= endOfWeek) {
+            days.push(day.toDate());
+            day = day.clone().add(1, 'd');
+        }
+
+        return _jsx('div', {
+            className: 'selector-week'
+        }, void 0, days.map(function (day) {
+            return _jsx('p', {}, void 0, day, _ref2);
+        }));
+    }
+}
+
+class DateSelector extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            studios: []
+            viewMonth: false
+        };
+    }
+
+    render() {
+        let month = moment();
+        console.log(month.startOf("week").toDate());
+        return _jsx('div', {
+            className: 'date-selector'
+        }, void 0, _jsx('div', {
+            className: 'selector-cal'
+        }, void 0, this.state.viewMonth ? _jsx(Studio, {
+            startDate: moment().startOf('month'),
+            endDate: moment().startOf('month').add(6, 'd')
+        }) : _jsx(Studio, {
+            startDate: moment().startOf('month'),
+            endDate: moment().startOf('month').add(6, 'd')
+        })));
+    }
+}
+
+var _ref3 = _jsx('br', {});
+
+class StudioInfo extends React.Component {
+    render() {
+        return _jsx('div', {}, void 0, _jsx('h1', {}, void 0, this.props.data[this.props.selected].facultyName), _jsx('div', {}, void 0, this.props.data[this.props.selected].facultyInfo.split('\n').map(function (text) {
+            return _jsx('p', {}, void 0, text, _ref3);
+        })));
+    }
+}
+
+var _ref4 = _jsx(DateSelector, {});
+
+var _ref5 = _jsx(CalTimes, {});
+
+class StudioCalendar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            studios: [],
+            selected: 0,
+            viewAll: false
         };
     }
 
@@ -41,37 +194,33 @@ class Calendar extends React.Component {
         axios.get("../data/times.json").then(res => {
             const studios = res.data;
             console.log(studios);
-            this.setState({ studios });
+            this.setState({
+                studios: studios,
+                selected: 2,
+                viewAll: false
+            });
         });
     }
 
     render() {
-        return _jsx("div", {
-            className: "sched-main"
-        }, void 0, this.state.studios.map(function (studio, i) {
-            return _jsx(Studio, {
-                className: "sched-row",
-                data: studio
-            });
-        }));
+        return _jsx('div', {
+            className: 'app-inner'
+        }, void 0, _jsx('div', {
+            className: 'top-area'
+        }, void 0, this.state.studios.length > 0 && _jsx(StudioInfo, {
+            data: this.state.studios,
+            selected: this.state.selected,
+            viewAll: this.state.viewAll
+        }), _ref4), _jsx('div', {
+            className: 'schedule'
+        }, void 0, _jsx('div', {
+            className: 'sched-body'
+        }, void 0, _ref5, this.state.studios.length > 0 && _jsx(CalendarBody, {
+            data: this.state.studios,
+            selected: this.state.selected,
+            viewAll: this.state.viewAll
+        }))));
     }
-
 }
 
-ReactDOM.render(_jsx(Calendar, {}), document.getElementById('root'));
-
-function populatePage(data) {}
-
-function getData(str) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        times = this.responseText;
-        populatePage(times);
-    };
-    xmlhttp.open("GET", "../data/times.json", true);
-    xmlhttp.send();
-}
-
-getData();
-
-//# sourceMappingURL=main-min.js.map
+ReactDOM.render(_jsx(StudioCalendar, {}), document.getElementById('root'));
--- js/main.js
+++ js/main.js
@@ -1,38 +1,193 @@
 var times = [];
+var eventHeight = 30;
 
-moment().format();
+var startOfWeek = moment().startOf('week');
+var endOfWeek = moment().endOf('week');
 
-function CalEvent(props) {
-    let eventInfo;
-    if (props.time.status) {
-        eventInfo = <p>{props.time.statusString}</p>;
-    } else {
-        eventInfo = <p>{props.time.statusString}</p>;
+var days = [];
+var day = startOfWeek;
+
+while (day <= endOfWeek) {
+    days.push(day.toDate());
+    day = day.clone().add(1, 'd');
+}
+
+console.log(days);
+
+class CalEvent extends React.Component {
+    render () {
+        let eventInfo;
+        if (this.props.time.status) {
+            eventInfo = <p>{this.props.time.statusString}</p>;
+        } else {
+            eventInfo = <p>{this.props.time.statusString}</p>;
+        }
+        return (
+            <div style={this.props.style} className={`sched-row cal-event-box`}>
+                <div className={`cal-event ${this.props.time.statusString}`}>
+                    {eventInfo}
+                </div>
+            </div>
+        );
     }
-    return (
-        <div className={`sched-row cal-event ${props.time.statusString}`}>
-            {eventInfo}
-        </div>
-    );
 }
 
-function Studio(props) {
-  return (
-      <div className="studio-col">
-          {props.data.days[0].times.map(function(time, i){
-            if (i > 11)
-                return <CalEvent time={time} className={`sched-row ${time.status}`} status={time.status}/>
-          })}
-      </div>
-  );
+class Studio extends React.Component {
+    render() {
+        let scope = this;
+        let count = 1;
+        console.log(scope.props.data);
+        return (
+          <div className="studio-col">
+              {
+
+                this.props.data.days[0].times.map(function(time, i){
+                    if (i > 11) {
+                        if (i != scope.props.data.days[0].times.length-1) {
+                            if (scope.props.data.days[0].times[i+1].statusString != time.statusString || time.statusString == "Closed") {
+
+                                let eventStyle = {
+                                    height: (count*eventHeight + "px")
+                                };
+                                count = 1;
+
+                                return <CalEvent time={time} className={`sched-row ${time.status}`} status={time.status} style={eventStyle}/>
+                            }
+                            else {
+                                count++;
+                            }
+                        }
+                    }
+
+              })}
+          </div>
+      );
+    }
 }
 
-class Calendar extends React.Component {
+class CalendarBody extends React.Component {
+    render() {
+        return (
+          <div className="sched-main sched-col">
+            {this.props.viewAll ? (
+                this.props.data.map(function(studio, i){
+                    return <Studio className="sched-row" data={studio}/>
+                })
+            ) : (
+                <Studio className="sched-row" data={this.props.data[this.props.selected]}/>
+            )}
+
+          </div>
+        );
+    }
+
+}
+
+class CalTimes extends React.Component {
+    render () {
+        let calTimeFormat = moment().hour(6).minute(0);
+        let calTimeNum = 6;
+        let calTimes = [];
+
+        while (calTimeNum <= 24) {
+            calTimes.push({ "formatted": calTimeFormat.format("h:mma"), "number": calTimeNum})
+            calTimeNum = calTimeNum+0.5;
+            calTimeFormat.add(30, 'm');
+        }
+
+        return (
+            <div className="sched-col">
+                {calTimes.map(function(time, i){
+                    let hourType = !Number.isInteger(time.number) ? "half-hour" : "full-hour";
+                    return <div className={`time sched-row ${hourType}`} id={`${time.number}-time`}>{time.formatted}</div>
+                })}
+            </div>
+        )
+    }
+}
+
+class SelectorDay extends React.Component {
+    render () {
+        return (
+            <div className="day">
+
+            </div>
+        )
+    }
+}
+
+class SelectorWeek extends React.Component {
+    render () {
+        let startOfWeek = moment().startOf('week');
+        let endOfWeek = moment().endOf('week');
+
+        let days = [];
+        let day = this.props.startDate;
+
+        while (day <= endOfWeek) {
+            days.push(day.toDate());
+            day = day.clone().add(1, 'd');
+        }
+
+        return (
+            <div className="selector-week">
+                {days.map(function(day){
+                    return <p>{day}<br/></p>
+                })}
+            </div>
+        )
+    }
+}
+
+class DateSelector extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            studios: []
+            viewMonth: false
+        };
+    }
+
+    render () {
+        let month = moment();
+        console.log(month.startOf("week").toDate())
+        return (
+            <div className="date-selector">
+                <div className="selector-cal">
+                    {this.state.viewMonth ? (
+                            <Studio startDate={moment().startOf('month')} endDate={moment().startOf('month').add(6, 'd')}/>
+                        ) : (
+                            <Studio startDate={moment().startOf('month')} endDate={moment().startOf('month').add(6, 'd')}/>
+                        )}
+                </div>
+            </div>
+        )
+    }
+}
+
+class StudioInfo extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>{this.props.data[this.props.selected].facultyName}</h1>
+                <div>
+                    {this.props.data[this.props.selected].facultyInfo.split('\n').map(function(text){
+                        return <p>{text}<br/></p>
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+class StudioCalendar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            studios: [],
+            selected: 0,
+            viewAll: false
         };
     }
 
@@ -41,39 +196,38 @@ class Calendar extends React.Component {
         .then(res => {
             const studios = res.data;
             console.log(studios);
-            this.setState({ studios });
+            this.setState({
+                studios: studios,
+                selected: 2,
+                viewAll: false
+            });
         });
     }
 
     render() {
         return (
-          <div className="sched-main">
-            {this.state.studios.map(function(studio, i){
-                return <Studio className="sched-row" data={studio}/>
-            })}
-          </div>
+            <div className="app-inner">
+                <div className="top-area">
+                    {this.state.studios.length > 0 &&
+                        <StudioInfo data={this.state.studios} selected={this.state.selected} viewAll={this.state.viewAll}/>
+                    }
+                    <DateSelector />
+                </div>
+                <div className="schedule">
+                    <div className="sched-body">
+                        <CalTimes />
+                        {this.state.studios.length > 0 &&
+                            <CalendarBody data={this.state.studios} selected={this.state.selected} viewAll={this.state.viewAll}/>
+                        }
+                    </div>
+                </div>
+            </div>
         );
     }
-
 }
+
 
 ReactDOM.render(
-  <Calendar />,
+  <StudioCalendar />,
   document.getElementById('root')
 );
-
-function populatePage(data) {
-
-}
-
-function getData(str) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        times = this.responseText;
-        populatePage(times);
-    };
-    xmlhttp.open("GET", "../data/times.json", true);
-    xmlhttp.send();
-}
-
-getData();
