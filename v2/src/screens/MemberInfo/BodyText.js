10d036db2d4a7e66f8edcb8f1d4d12d35c29300f
Hunter Caron <huntercaron@mac.com>
2017-04-04 15:14:49 -0400

    cleaning up membership page

--- v2/src/screens/MemberInfo/BodyText.js
+++ v2/src/screens/MemberInfo/BodyText.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import styled               from 'styled-components';
+
+const Info = styled.div`
+    margin: auto;
+    max-width: 600px;
+
+    h3 {
+        margin-top: 40px;
+        margin-bottom: 8px;
+        font-size: 2.4rem;
+        font-weight: 600;
+    }
+
+    li {
+        margin-bottom: 18px;
+    }
+
+    p {
+        margin-bottom: 18px;
+    }
+`
 
 const BodyText = (props) => {
     return (
-        <div {...props}>
+        <Info {...props}>
 
-        </div>
+        </Info>
     )
 }
 
